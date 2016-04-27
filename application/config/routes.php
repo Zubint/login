dda4bd3f903430dab5a66e9ece632825ca3679da
Zubin Talib <zubintalib@izooky.com>
2016-04-27 10:10:34 -0500

    updated routes and changed file names to lower case

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$route['default_controller'] = "/Users";
+$route['default_controller'] = "/users";
 $route['404_override'] = '';
 
 
